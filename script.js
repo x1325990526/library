4df6afe4e426e881989c0c762aae35e8bb017885
Nova <64618129+x1325990526@users.noreply.github.com>
2026-08-21 14:22:09 +0800

    feat: implement addBookToLibrary
    - Auto-generate book id
    - Default isRead to false
    - Add three sample books

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const myLibrary = [];
 
-function Book(id, title, author, pages, isRead) {
-    this.id = id;
+addBookToLibrary("Grimms' Fairy Tales", "Brothers Grimm", 200);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310);
+addBookToLibrary("1984", "George Orwell", 328);
+
+
+function Book(title, author, pages, isRead=false) {
+
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -12,7 +17,10 @@ function Book(id, title, author, pages, isRead) {
     };
 
 }
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages) {
   // take params, create a book then store it in the array
+    const book = new Book(title, author, pages);
+    book.id = crypto.randomUUID();
+    myLibrary.push(book);
   
 }
